7e42ccdc31da345ed9a453b7ca4e3c453614d0d8
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-08 19:06:40 +0530

    create custom widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -16,17 +20,22 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReuseableCard(colour: Color(0xFF1D1E33),),),
-              Expanded(child: ReuseableCard(colour: Color(0xFF1D1E33),),),
+              Expanded(child: ReuseableCard(colour: activeCardColor,),),
+              Expanded(child: ReuseableCard(colour: activeCardColor,),),
             ],
           )),
-          Expanded(child: ReuseableCard(colour: Color(0xFF1D1E33),),),
+          Expanded(child: ReuseableCard(colour: activeCardColor,),),
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReuseableCard(colour: Color(0xFF1D1E33),),),
-              Expanded(child: ReuseableCard(colour: Color(0xFF1D1E33),),),
-            ],
-          ),),],
+              Expanded(child: ReuseableCard(colour: activeCardColor,),),
+              Expanded(child: ReuseableCard(colour: activeCardColor,),),],),),
+        Container(
+          color: bottomContainerColor,
+          margin: EdgeInsets.only(top: 10.0),
+          width: double.infinity,
+          height: bottomContainerHeight,
+        )
+        ],
       ),
     );
   }
@@ -34,12 +43,14 @@ class _InputPageState extends State<InputPage> {
 
 class ReuseableCard extends StatelessWidget {
 
-  ReuseableCard({@required this.colour});
-  Color colour;
+  ReuseableCard({@required this.colour, this.cardChild}) ;
+  final Color colour;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
