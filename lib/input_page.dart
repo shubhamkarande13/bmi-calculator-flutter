3bb41250d2a0f14a72917708f07a3c44629bd95f
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-09 16:35:22 +0530

    fix gender selector

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -7,12 +7,39 @@ import 'reuseable_card.dart';
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
 const bottomContainerColor = Color(0xFFEB1555);
+const inactiveCardColor = Color(0xFF111328);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Color maleCardColour = inactiveCardColor;
+  Color femaleCardColour = inactiveCardColor;
+
+  void updateColour (int gender) {
+    //male card pressed
+  if (gender==1) {
+      if (maleCardColour == inactiveCardColor) {
+        maleCardColour = activeCardColor;
+        femaleCardColour=inactiveCardColor;
+      }
+      else {
+       maleCardColour = inactiveCardColor;
+      }
+    }
+  if(gender==2){
+    if (femaleCardColour == inactiveCardColor) {
+      femaleCardColour = activeCardColor;
+      maleCardColour = inactiveCardColor;
+    }
+    else {
+      femaleCardColour = inactiveCardColor;
+    }  }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +50,21 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReuseableCard(colour: activeCardColor,
-              cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE',),),),
-              Expanded(child: ReuseableCard(colour: activeCardColor,cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),),
+              Expanded(child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    updateColour(1);
+                  });
+                },
+                child: ReuseableCard(colour: maleCardColour,
+                cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE',),),
+              ),),
+              Expanded(child: GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      updateColour(2);
+                    });
+                  },child: ReuseableCard(colour: femaleCardColour,cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),)),
               ),],
           )),
           Expanded(child: ReuseableCard(colour: activeCardColor,),),
